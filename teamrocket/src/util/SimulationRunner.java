aa493c2ff71da8d8f0aeaac99d71eb40b1ace6e2
huckerlaur <huckerlaur@ecs.vuw.ac.nz>
2016-10-27 19:04:41 +1300

    Attempt to get correct coords for Monte Carlo simulations

--- teamrocket/src/util/SimulationRunner.java
+++ teamrocket/src/util/SimulationRunner.java
@@ -3,13 +3,17 @@ package util;
 import data.SimulationData;
 import net.sf.openrocket.document.OpenRocketDocument;
 import net.sf.openrocket.document.Simulation;
-import net.sf.openrocket.simulation.FlightData;
-import net.sf.openrocket.simulation.FlightDataBranch;
-import net.sf.openrocket.simulation.SimulationOptions;
+import net.sf.openrocket.models.wind.PinkNoiseWindModel;
+import net.sf.openrocket.models.wind.WindModel;
+import net.sf.openrocket.motor.MotorInstanceConfiguration;
+import net.sf.openrocket.simulation.*;
 import net.sf.openrocket.simulation.exception.SimulationException;
 import net.sf.openrocket.simulation.listeners.AbstractSimulationListener;
 import net.sf.openrocket.simulation.listeners.SimulationListener;
 import net.sf.openrocket.util.ArrayList;
+import net.sf.openrocket.util.Coordinate;
+import net.sf.openrocket.util.GeodeticComputationStrategy;
+import net.sf.openrocket.util.WorldCoordinate;
 import org.json.simple.JSONObject;
 
 import static net.sf.openrocket.simulation.FlightDataType.TYPE_LATITUDE;
@@ -56,7 +60,6 @@ public class SimulationRunner {
         double angleStep = 1.0;
 
         for (Simulation simulation : openRocketDocument.getSimulations()) {
-            setWeatherConditions(this.weatherDataJson, simulation.getOptions());
             ArrayList<FlightData> flightData = simulateMultipleLaunchAngles(simulation, startAngle, endAngle, angleStep);
             simulationDataList.add(new SimulationData(simulation, flightData));
         }
@@ -72,8 +75,12 @@ public class SimulationRunner {
 
             // Perform the simulation for the specified range of launch angles
             for(double launchAngle = startAngle; launchAngle <= endAngle; launchAngle += angleStep) {
+
                 // Update simulation options
                 simulationOptions.setLaunchRodAngle(Math.toRadians(launchAngle));
+                simulation.setSimulationOptions(simulationOptions);
+
+                simulationOptions = setWeatherConditions(this.weatherDataJson, simulation.getOptions());
                 simulation.setSimulationOptions(simulationOptions);
 
                 // Perform the simulation
@@ -82,9 +89,15 @@ public class SimulationRunner {
                 // Get the result of the simulation and set launch angle to be stored with flight data
                 FlightData f = simulation.getSimulatedData();
                 f.setLaunchAngle(launchAngle);
-
+                f.setLandingCoord(getLandingCoord2(simulation));
                 flightDataList.add(f);
-                f.setLandingCoord(getLandingCoord(simulation));
+
+                System.out.println();
+                System.out.println(getLandingCoord(simulation));
+                System.out.println(getLandingCoord2(simulation));
+                System.out.println(getLandingCoord3(simulation));
+                System.out.println();
+
             }
         } catch (SimulationException e) {
             System.err.println(e.getMessage());
@@ -93,7 +106,7 @@ public class SimulationRunner {
         return flightDataList;
     }
 
-    public void setWeatherConditions(JSONObject conditions, SimulationOptions simulationOptions){
+    public SimulationOptions setWeatherConditions(JSONObject conditions, SimulationOptions simulationOptions){
 
         JSONObject main = (JSONObject)conditions.get("main");
         JSONObject wind = (JSONObject)conditions.get("wind");
@@ -105,6 +118,8 @@ public class SimulationRunner {
         simulationOptions.setWindDirection((double) wind.get("deg"));
         simulationOptions.setLaunchPressure((double) main.get("pressure"));
         simulationOptions.setLaunchTemperature((double) main.get("temp"));
+
+        return simulationOptions;
         //simulationOptions.setLaunchIntoWind(true); //Do we wanna do this??
     }
 
@@ -113,10 +128,64 @@ public class SimulationRunner {
         double initialLatitude = (double)((JSONObject)weatherDataJson.get("coord")).get("lat");
         double initialLongitude = (double)((JSONObject)weatherDataJson.get("coord")).get("lon");
 
+        SimulationStatus status = new SimulationStatus(simulation.getConfiguration(), new MotorInstanceConfiguration(), (simulation.getSimulatedConditions().toSimulationConditions()));
+        WorldCoordinate coord = status.getRocketWorldPosition();
+        Coordinate launchCoord = new Coordinate(initialLongitude, initialLatitude);
+        GeodeticComputationStrategy geodeticComputation = (simulation.getSimulatedConditions().toSimulationConditions()).getGeodeticComputation();
+
+        WorldCoordinate landingCoord = geodeticComputation.addCoordinate(coord, launchCoord);
+        WorldCoordinate diffCoord = differenceCoord(coord, landingCoord);
+
+        System.out.println();
+        System.out.println("angle: " + Math.toDegrees(simulation.getOptions().getLaunchRodAngle()));
+        System.out.println("wind speed: " + simulation.getOptions().getWindSpeedAverage());
+        System.out.println(coord);
+        System.out.println(landingCoord);
+        System.out.println(diffCoord);
+        System.out.println(coordToMeters(diffCoord));
+        System.out.println();
+
+        return coordToMeters(diffCoord);
+    }
+
+    public String getLandingCoord2(Simulation simulation){
+        double initialLatitude = (double)((JSONObject)weatherDataJson.get("coord")).get("lat");
+        double initialLongitude = (double)((JSONObject)weatherDataJson.get("coord")).get("lon");
+
         FlightDataBranch flightDataBranch = simulation.getSimulatedData().getBranch(0);
 
-        return String.format("(%.2f,%.2f)", (flightDataBranch.getLast(TYPE_LONGITUDE) - initialLongitude),
+        Coordinate launchCoord = new Coordinate(initialLongitude, initialLatitude);
+        WorldCoordinate coord = new WorldCoordinate(flightDataBranch.getLast(TYPE_LONGITUDE), flightDataBranch.getLast(TYPE_LATITUDE), 0);
+
+        GeodeticComputationStrategy geodeticComputation = (simulation.getSimulatedConditions().toSimulationConditions()).getGeodeticComputation();
+        WorldCoordinate landingCoord = geodeticComputation.addCoordinate(coord, launchCoord);
+        WorldCoordinate diffCoord = differenceCoord(coord, landingCoord);
+
+        return coordToMeters(diffCoord);
+    }
+
+    public String getLandingCoord3(Simulation simulation){
+        //TODO: check these values are legit
+        double initialLatitude = (double)((JSONObject)weatherDataJson.get("coord")).get("lat");
+        double initialLongitude = (double)((JSONObject)weatherDataJson.get("coord")).get("lon");
+
+        FlightDataBranch flightDataBranch = simulation.getSimulatedData().getBranch(0);
+
+        return String.format("(%f,%f)", (flightDataBranch.getLast(TYPE_LONGITUDE) - initialLongitude),
                 (flightDataBranch.getLast(TYPE_LATITUDE) - initialLatitude));
     }
 
+    public WorldCoordinate differenceCoord(WorldCoordinate c1, WorldCoordinate c2){
+        double dlat = c2.getLatitudeDeg() - c1.getLatitudeDeg();
+        double dlon = c2.getLongitudeDeg() - c1.getLongitudeDeg();
+        return new WorldCoordinate(dlat, dlon, 0);
+    }
+
+    public String coordToMeters(WorldCoordinate c){
+        double METERS_PER_DEGREE_LATITUDE = 111325; // "standard figure"
+        double METERS_PER_DEGREE_LONGITUDE_EQUATOR = 111050;
+        double earthRadius = 6371000; //meters
+        return String.format("(%f,%f)", c.getLongitudeDeg() * METERS_PER_DEGREE_LONGITUDE_EQUATOR, c.getLatitudeDeg() * METERS_PER_DEGREE_LATITUDE);
+        //return String.format("(%f,%f)", c.getLongitudeDeg()*1000, c.getLatitudeDeg()*1000);
+    }
 }
